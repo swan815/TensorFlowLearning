4ddb798168c7e01d29172390eb6b5d7c0598e3a6
swan815 <30940660@qq.com>
2018-05-30 11:26:34 +0800

    Simple example

---
+++ graphConstruction.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+
+# TensorFlow是编程系统，使用图来表示编程任务，图中的节点被称为op，下面的constant()和matmul()也被称为op
+
+matrix1 = tf.constant([[3., 3.]])
+
+matrix2 = tf.constant([[2.], [2.]])
+
+product = tf.matmul(matrix1, matrix2)
+
+# 构造完图后，必须启动图
+
+sess = tf.Session()
+
+# 调用 sess 的 'run()' 方法来执行矩阵乘法 op, 传入 'product' 作为该方法的参数.
+# 上面提到, 'product' 代表了矩阵乘法 op 的输出, 传入它是向方法表明, 我们希望取回
+# 矩阵乘法 op 的输出.
+#
+# 整个执行过程是自动化的, 会话负责传递 op 所需的全部输入. op 通常是并发执行的.
+#
+# 函数调用 'run(product)' 触发了图中三个 op (两个常量 op 和一个矩阵乘法 op) 的执行.
+#
+# 返回值 'result' 是一个 numpy `ndarray` 对象.
+
+result = sess.run(product)
+
+print(result)
+
+sess.close()
+
+# TensorFlow 将图形定义转换成分布式执行的操作, 以充分利用可用的计算资源(如 CPU 或 GPU).
+# 一般你不需要显式指定使用 CPU 还是 GPU, TensorFlow 能自动检测. 如果检测到 GPU, TensorFlow 会尽可能地利用找到的第一个 GPU 来执行操作.
+
+# with tf.Session() as sess:
+#   with tf.device("/gpu:1"):
+#     matrix1 = tf.constant([[3., 3.]])
+#     matrix2 = tf.constant([[2.],[2.]])
+#     product = tf.matmul(matrix1, matrix2)
+#     ...
--- kmeans.py
+++ kmeans.py
@@ -1,2 +1,39 @@
 from __future__ import print_function
 
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib.factorization import KMeans
+
+# 忽略所有的GPUs，tf的随机森林不需要
+
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+# 导入 MNIST data
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data", one_hot=True)
+full_data_x = mnist.train.images
+
+# 变量
+# 训练的步骤
+num_steps = 50
+# 每一个batch的样本大小
+batch_size = 1024
+# 聚类的数目
+k = 25
+# 0-9十个数字
+num_classes = 10
+# 每一个图像都是28*28像素
+
+# 输入图像
+X = tf.placeholder(tf.float32, shape=[None, num_features])
+# 标签
+Y = tf.placeholder(tf.float32, shape=[None, num_features])
+
+# K-Means聚类的参数
+kmeans = KMeans(inputs=X, num_clusters=k,
+                distance_metric='cosine', use_mini_batch=True)
+# 建立KMeans计算图
+training_graph = kmeans.training_graph()
+if len(training_graph) > 6:
